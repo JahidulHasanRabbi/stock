97d1a87f90467461dcbf61c44824bc84b2cae012
Rabbie <hasanrabbie@gmail.com>
2024-02-13 02:46:05 +0600

    J

--- server/models.py
+++ server/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class StockModel(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     date = models.DateField()
     trade_code = models.CharField(max_length=20)
     high = models.DecimalField(max_digits=10, decimal_places=2)
