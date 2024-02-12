580cc9914d996b744a7640b8786c88b87424b632
Rabbie <hasanrabbie@gmail.com>
2024-02-13 02:04:10 +0600

    FINAL

--- tast_ai_intern/settings.py
+++ tast_ai_intern/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-8*^6%+(wa6%b-b7l5jv2vp@p@h4$%(m*sft3^nl2sqkj7)4ul4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
